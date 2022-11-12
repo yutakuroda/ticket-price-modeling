929ea68bcb86f240bf5fbe1dd483310b5d61310f
yutakuroda <kurodayuta4696@gmail.com>
2022-11-12 17:25:29 +0900

    年齢が不正な場合のエラーを修正

--- src/domain/customer/age.ts
+++ src/domain/customer/age.ts
@@ -1,5 +1,5 @@
 export class Age {
   constructor(public readonly value: number) {
-    if (value < 0 || value > 200) throw new RangeError();
+    if (value < 0 || value > 200) throw new Error("年齢が不正です");
   }
 }
