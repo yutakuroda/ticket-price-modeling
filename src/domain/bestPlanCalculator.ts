8266917ccd4051e13f50ce9171bf636eceec35d3
yutakuroda <kurodayuta4696@gmail.com>
2022-11-12 16:08:22 +0900

    リファクタリング

--- src/domain/bestPlanCalculator.ts
+++ src/domain/bestPlanCalculator.ts
@@ -1,17 +1,25 @@
 import { allPlans } from "domain/plan/allPlans";
 import { Customer } from "domain/customer/customer";
+import { Plan } from "domain/plan/plan";
 
 export class BestPlanCalculator {
   static calculate(cunstomer: Customer, now: Date): any {
-    const availablePlans = allPlans.filter((plan) =>
-      plan.isAvailable(cunstomer)
-    );
-
-    const bestPricePlan = availablePlans.find(
-      (plan) =>
-        plan.price() === Math.min(...availablePlans.map((plan) => plan.price()))
-    );
+    const availablePlans = this.filterAvailablePlans(allPlans, cunstomer);
+    const bestPricePlan = this.findBestPricePlan(availablePlans, now);
 
     return bestPricePlan;
+  }
+
+  private static filterAvailablePlans(
+    allPlans: Plan[],
+    cunstomer: Customer
+  ): Plan[] {
+    return allPlans.filter((plan) => plan.isAvailable(cunstomer));
+  }
+
+  private static findBestPricePlan(availablePlans: Plan[], now: Date): Plan {
+    return availablePlans.reduce((prev, current) => {
+      return prev.price(now) <= current.price(now) ? prev : current;
+    });
   }
 }
--- src/domain/plan/plan.d.ts
+++ src/domain/plan/plan.d.ts
@@ -2,5 +2,5 @@ import { Customer } from "domain/customer/customer";
 
 export interface Plan {
   isAvailable(customer: Customer): boolean;
-  price(): number;
+  price(now): number;
 }
