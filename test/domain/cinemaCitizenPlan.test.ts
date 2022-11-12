b791a583ab1cb26a6bd3f22832f269fa7cd9c1c7
yutakuroda <kurodayuta4696@gmail.com>
2022-11-12 21:00:38 +0900

    cinemaCitizenPlan#isAvailableのテストケースを追加。現在日時に応じて価格を判定する。

--- test/domain/cinemaCitizenPlan.test.ts
+++ test/domain/cinemaCitizenPlan.test.ts
@@ -1,3 +1,5 @@
+import { addBusinessDays, isFirstDayOfMonth, nextSaturday } from "date-fns";
+
 import {
   Age,
   CINEMA_CITIZEN_CATEGORY,
@@ -45,9 +47,65 @@ describe("CinemaCitizenPlan", () => {
     });
   });
 
-  it(".price", () => {
-    const now = new Date();
+  describe(".price", () => {
+    test("映画の日で、平日20時までの場合、1000円を返す", () => {
+      const cinemaWeekday = new Date(2022, 11, 1, 19, 59, 59);
 
-    expect(plan.price(now)).toBe(1000);
+      expect(plan.price(cinemaWeekday)).toBe(1000);
+    });
+
+    test("映画の日で、平日20時以降の場合、1000円を返す", () => {
+      const cinemaWeekday = new Date(2022, 11, 1, 20, 20, 20);
+
+      expect(plan.price(cinemaWeekday)).toBe(1000);
+    });
+
+    test("映画の日で、土日20時までの場合、1200円を返す", () => {
+      const cinemaSaturday = new Date(2022, 10, 1, 19, 59, 59);
+
+      expect(plan.price(cinemaSaturday)).toBe(1200);
+    });
+
+    test("映画の日で、土日20時以降の場合、1000円を返す", () => {
+      const cinemaSaturday = new Date(2022, 10, 1, 20, 0, 0);
+
+      expect(plan.price(cinemaSaturday)).toBe(1000);
+    });
+
+    test("映画の日ではなく、平日20時までの場合、1000円を返す。ただし祝日は考慮していない", () => {
+      const weekday = addBusinessDays(new Date(), 1);
+      if (isFirstDayOfMonth(weekday)) addBusinessDays(weekday, 1);
+
+      weekday.setHours(19, 59, 59);
+
+      expect(plan.price(weekday)).toBe(1000);
+    });
+
+    test("映画の日ではなく、平日20時以降の場合、1000円を返す。ただし祝日は考慮していない", () => {
+      const weekday = addBusinessDays(new Date(), 1);
+      if (isFirstDayOfMonth(weekday)) addBusinessDays(weekday, 1);
+
+      weekday.setHours(20, 0, 0);
+
+      expect(plan.price(weekday)).toBe(1000);
+    });
+
+    test("映画の日ではなく、土日20時までの場合、1300円を返す。ただし祝日は考慮していない", () => {
+      const saturday = nextSaturday(new Date());
+      if (isFirstDayOfMonth(saturday)) nextSaturday(saturday);
+
+      saturday.setHours(19, 59, 59);
+
+      expect(plan.price(saturday)).toBe(1300);
+    });
+
+    test("映画の日ではなく、土日20時以降の場合、1000円を返す。ただし祝日は考慮していない", () => {
+      const saturday = nextSaturday(new Date());
+      if (isFirstDayOfMonth(saturday)) nextSaturday(saturday);
+
+      saturday.setHours(20, 0, 0);
+
+      expect(plan.price(saturday)).toBe(1000);
+    });
   });
 });
