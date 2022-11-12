62fa451ebbda6c2684f4c987c64e3cfa77feb80d
yutakuroda <kurodayuta4696@gmail.com>
2022-11-12 14:33:07 +0900

    リファクタリング

--- src/domain/customer.ts
+++ src/domain/customer.ts
@@ -2,17 +2,9 @@ import { CINEMA_CITIZEN } from "domain/cinemaCitizen";
 import { DISABILITY } from "domain/disability";
 
 export class Customer {
-  age: number;
-  cinemaCitizen: CINEMA_CITIZEN;
-  disability: DISABILITY;
-
   constructor(
-    age: number,
-    cinemaCitizen: CINEMA_CITIZEN,
-    disability: DISABILITY
-  ) {
-    this.age = age;
-    this.cinemaCitizen = cinemaCitizen;
-    this.disability = disability;
-  }
+    public age: number,
+    public cinemaCitizen: CINEMA_CITIZEN,
+    public disability: DISABILITY
+  ) {}
 }
