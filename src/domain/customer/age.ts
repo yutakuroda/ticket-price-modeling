b65bb3a1dd9d5b4aacaa0bd9953fda87af6f8c67
yutakuroda <kurodayuta4696@gmail.com>
2022-11-16 22:23:59 +0900

    マジックナンバーを定数に変更

--- src/domain/customer/age.ts
+++ src/domain/customer/age.ts
@@ -1,5 +1,9 @@
 export class Age {
+  readonly MINIMUM_AGE = 0;
+  readonly MAXIMUM_AGE = 200;
+
   constructor(public readonly value: number) {
-    if (value < 0 || value > 200) throw new Error("年齢が不正です");
+    if (value < this.MINIMUM_AGE) throw new Error("年齢が不正です");
+    if (value > this.MAXIMUM_AGE) throw new Error("年齢が不正です");
   }
 }
