4e8886bd8e13242b2aed6594c5780247ed487f08
yutakuroda <kurodayuta4696@gmail.com>
2022-11-16 22:39:43 +0900

    ageのvalidationを追加

--- src/domain/customer/age.ts
+++ src/domain/customer/age.ts
@@ -3,6 +3,7 @@ export class Age {
   readonly MAXIMUM_AGE = 200;
 
   constructor(public readonly value: number) {
+    if (!Number.isInteger(value)) throw new Error("年齢が不正です");
     if (value < this.MINIMUM_AGE) throw new Error("年齢が不正です");
     if (value > this.MAXIMUM_AGE) throw new Error("年齢が不正です");
   }
--- test/domain/customer/age.test.ts
+++ test/domain/customer/age.test.ts
@@ -2,15 +2,19 @@ import { Age } from "domain/customer";
 
 describe("Age", () => {
   describe("new", () => {
-    test("0未満の数字が与えられた場合、エラーになる", () => {
+    test("小数値が与えられた場合、エラーになる", () => {
+      expect(() => new Age(1.5)).toThrowError("年齢が不正です");
+    });
+
+    test("0未満の整数が与えられた場合、エラーになる", () => {
       expect(() => new Age(-1)).toThrowError("年齢が不正です");
     });
 
-    test("201以上の数字が与えられた場合、エラーになる", () => {
+    test("201以上の整数が与えられた場合、エラーになる", () => {
       expect(() => new Age(201)).toThrowError("年齢が不正です");
     });
 
-    test("0以上200以下の数字が与えられた場合、エラーにならない", () => {
+    test("0以上200以下の整数が与えられた場合、エラーにならない", () => {
       expect(new Age(0)).toBeInstanceOf(Age);
       expect(new Age(200)).toBeInstanceOf(Age);
     });
