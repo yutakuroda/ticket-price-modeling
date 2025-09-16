1f1fc150e1977335f7f48db948bbf09e200f8a28
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-09-17 00:28:06 +0900

    fix type. MINIMUM_AGE to MINIMUM_PRICE

--- src/domain/plan/price.ts
+++ src/domain/plan/price.ts
@@ -1,9 +1,8 @@
 export class Price {
-  readonly MINIMUM_AGE = 0;
-  readonly MAXIMUM_AGE = 200;
+  readonly MINIMUM_PRICE = 0;
 
   constructor(public readonly value: number) {
     if (!Number.isInteger(value)) throw new Error("価格が不正です");
-    if (value < this.MINIMUM_AGE) throw new Error("価格が不正です");
+    if (value < this.MINIMUM_PRICE) throw new Error("価格が不正です");
   }
 }
